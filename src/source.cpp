5c9e223aa19b3835fd12c866a803d58b715baf07
ʀᴀᴅıcᴀʟ ɳınյᴀ <68967924+Xinixter@users.noreply.github.com>
2021-04-17 14:01:43 +0530

    Init complete

--- src/source.cpp
+++ src/source.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+
 int main()
 {
 	std::cout << "Let's start!...";
+
+	GLFWwindow* sampleWindow;
+
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		exit(EXIT_FAILURE);
+	}
+
+	sampleWindow = glfwCreateWindow(500, 500, "Sample", NULL, NULL);
+
+	if (!sampleWindow) {
+		fprintf(stderr, "Failed to create GLFW Winow\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	while (!glfwWindowShouldClose(sampleWindow))
+	{
+		glfwSwapBuffers(sampleWindow);
+		glfwPollEvents();
+		
+	}
+	
+	glfwTerminate();
+
+	return 0;
 }
