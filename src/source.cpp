b4c544fc4d3df18ec5d51b6db101695e7e0fc98b
ʀᴀᴅıcᴀʟ ɳınյᴀ <68967924+Xinixter@users.noreply.github.com>
2021-09-11 21:03:09 +0530

    GLAD setup

--- src/source.cpp
+++ src/source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main()
@@ -20,10 +21,18 @@ int main()
 	}
 
 	glfwMakeContextCurrent(sampleWindow);
+	glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
 
 	while (!glfwWindowShouldClose(sampleWindow)) {
 
-		GLuint vertexArrayID;
+		// GLuint
+		// glGenVertexArrays();
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 
 		glfwSwapBuffers(sampleWindow);
 		glfwPollEvents();
